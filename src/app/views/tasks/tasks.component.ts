b38073199fb05f72b7d7758160bb36e6917f12f9
Gennady Glushenkov <gennadiyglushenkov@gmail.com>
2023-06-28 12:58:53 +0700

    Начало переноса отображения задач в Material Table

--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {DataHandlerService} from "../../service/data-handler.service";
 import {Task} from "../../model/Task";
+import {MatTableDataSource} from "@angular/material/table";
 
 @Component({
   selector: 'app-tasks',
@@ -8,18 +9,37 @@ import {Task} from "../../model/Task";
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit{
+  displayedColumns: string[] = ['color', 'id', 'title', 'date', 'priority', 'category'];
+  dataSource: MatTableDataSource<Task>;
 
-  tasks: Task[] | null = null;
+  tasks: Task[] = [];
 
   constructor(private dataHandler: DataHandlerService) {
+    this.dataSource = new MatTableDataSource();
   }
 
   ngOnInit() {
     this.dataHandler.tasksSubject.subscribe(tasks => this.tasks = tasks);
+    //this.dataSource = new MatTableDataSource();
+
+    this.refreshTable();
   }
 
   toggleTaskCompleted(task: Task) {
     task.completed = !task.completed;
+  }
+
+  getPriorityColor(task: Task): string {
+
+    if(task.priority && task.priority.color) {
+      return task.priority.color;
+    }
+
+    return '#fff';
+  }
+
+  private refreshTable() {
+    this.dataSource.data = this.tasks;
   }
 
 }
