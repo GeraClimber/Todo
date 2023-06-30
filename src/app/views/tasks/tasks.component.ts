f829c783e99a1d5dad63cff48394aea0b754c487
Gennady Glushenkov <gennadiyglushenkov@gmail.com>
2023-06-30 23:37:32 +0700

    Добавил сотрировку и пагинацию в таблицу

--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -1,14 +1,20 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {DataHandlerService} from "../../service/data-handler.service";
 import {Task} from "../../model/Task";
-import {MatTableDataSource} from "@angular/material/table";
+import {MatTable, MatTableDataSource} from "@angular/material/table";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
-export class TasksComponent implements OnInit{
+export class TasksComponent implements OnInit, AfterViewInit {
+
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator | null = null;
+  @ViewChild(MatSort, { static: true }) sort: MatSort | null = null;
+
   displayedColumns: string[] = ['color', 'id', 'title', 'date', 'priority', 'category'];
   dataSource: MatTableDataSource<Task>;
 
@@ -19,10 +25,14 @@ export class TasksComponent implements OnInit{
   }
 
   ngOnInit() {
-    this.dataHandler.tasksSubject.subscribe(tasks => this.tasks = tasks);
-    //this.dataSource = new MatTableDataSource();
+    this.dataHandler.tasksSubject.subscribe(tasks => {
+      this.tasks = tasks;
+      this.refreshTable();
+    });
+  }
 
-    this.refreshTable();
+  ngAfterViewInit() {
+    this.addTableObjects();
   }
 
   toggleTaskCompleted(task: Task) {
@@ -44,6 +54,12 @@ export class TasksComponent implements OnInit{
 
   private refreshTable() {
     this.dataSource.data = this.tasks;
+    this.addTableObjects();
+  }
+
+  private addTableObjects() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
 }
