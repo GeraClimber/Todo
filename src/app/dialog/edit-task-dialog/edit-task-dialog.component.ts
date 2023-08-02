6cbbac88044384b7ff4940fefbaf84c5a9f36cd1
Gennady Glushenkov <gennadiyglushenkov@gmail.com>
2023-08-02 13:55:50 +0700

    редактирование категории в диалоговом окне

--- src/app/dialog/edit-task-dialog/edit-task-dialog.component.ts
+++ src/app/dialog/edit-task-dialog/edit-task-dialog.component.ts
@@ -1,6 +1,8 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import {Task} from "../../model/Task";
+import {DataHandlerService} from "../../service/data-handler.service";
+import {Category} from "../../model/Category";
 
 @Component({
   selector: 'app-edit-task-dialog',
@@ -12,7 +14,7 @@ export class EditTaskDialogComponent implements OnInit {
   constructor(
     private dialogRef: MatDialogRef<EditTaskDialogComponent>,
     @Inject(MAT_DIALOG_DATA) private data: [Task, string],
-
+    private dataHandler: DataHandlerService
     ) {
   }
 
@@ -20,17 +22,23 @@ export class EditTaskDialogComponent implements OnInit {
   task: Task = this.data[0];
 
   tmpTitle: string="";
+  tmpCategory: Category | undefined = undefined;
+  categories: Category[] = [];
 
 
   ngOnInit() {
 
     this.dialogTitle = this.data[1];
     this.tmpTitle = this.task.title;
+    this.tmpCategory = this.task.category;
+
+    this.dataHandler.getAllCategories().subscribe(items => this.categories = items);
   }
 
   onConfirm(): void {
 
       this.task.title = this.tmpTitle;
+      this.task.category = this.tmpCategory;
       this.dialogRef.close(this.task);
   }
 
